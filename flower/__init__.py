6f4d0c66cc2aff256f3f9f8e25327cb45ddedc91
Rockallite Wulf <rockallite.wulf@gmail.com>
2017-03-30 09:55:55 +0800

    Add development version number "dev1". Also fixes setup.py version parsing

--- flower/__init__.py
+++ flower/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 
-VERSION = (1, 0, 0)
-__version__ = '.'.join(map(str, VERSION)) + '-dev'
+dev1 = 'dev1'
+VERSION = (1, 0, 0, dev1)
+__version__ = '.'.join(map(str, VERSION)) + '-' + dev1
