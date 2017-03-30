def00f68245b1208ed4f65c1004d3105563aa073
Rockallite Wulf <rockallite.wulf@gmail.com>
2017-03-30 13:15:41 +0800

    Bump version to 1.0.0.dev2

--- flower/__init__.py
+++ flower/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-dev1 = 'dev1'
-VERSION = (1, 0, 0, dev1)
-__version__ = '.'.join(map(str, VERSION)) + '-' + dev1
+dev2 = 'dev2'
+VERSION = (1, 0, 0, dev2)
+__version__ = '.'.join(map(str, VERSION))
